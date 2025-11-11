469c4f442836a88e243af5c49f84ada2b5a758d4
ana.barbosa <anahelenaa12@gmail.com>
2025-11-11 09:19:06 -0300

    Criando nova classe usando IO

---
+++ src/main/java/br/ufpb/dcx/barbosa/ana/TesteAna.java
@@ -0,0 +1,10 @@
+package br.ufpb.dcx.barbosa.ana;
+
+public class TesteAna {
+    static void main(){
+        IO.println("Olá Mundo!!!");
+        String nome= IO.readln("Digite seu nome ");
+        IO.println("Olá "+  nome);
+
+    }
+}
